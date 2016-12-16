d5628f9124325a87a24a4e19d28497f1cab25753
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-12-16 19:40:01 +0900

    update index page

--- src/Resource/Page/Index.php
+++ src/Resource/Page/Index.php
@@ -2,13 +2,28 @@
 
 namespace MyVendor\MyRedux\Resource\Page;
 
+use BEAR\Resource\RenderInterface;
 use BEAR\Resource\ResourceObject;
+use Ray\Di\Di\Inject;
+use Ray\Di\Di\Named;
 
 class Index extends ResourceObject
 {
+    /**
+     * @Inject
+     * @Named("ssr_hello")
+     */
+    public function setRenderer(RenderInterface $renderer)
+    {
+        parent::setRenderer($renderer);
+    }
+
     public function onGet($name = 'BEAR.Sunday')
     {
-        $this['greeting'] = 'Hello ' . $name;
+        $this->body = [
+            'title' => 'To ' . $name,
+            'hello' => ['message' => 'Hello ' . $name]
+        ];
 
         return $this;
     }
